00ce0dc668d21832bf395438544fedb0cf6d28e0
Monika <monika.tomczyk@hotmail.com>
2022-01-26 01:23:00 +0100

    APIready

--- app/src/main/java/com/example/currency_converter/FirstFragment.kt
+++ app/src/main/java/com/example/currency_converter/FirstFragment.kt
@@ -1,65 +1,71 @@
 package com.example.currency_converter
 
-import android.app.DownloadManager
 import android.os.Bundle
-import android.text.Editable
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.view.isVisible
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.example.currency_converter.databinding.FragmentFirstBinding
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.Response
-import kotlin.math.round
+import okhttp3.*
+import okio.IOException
+import org.json.JSONObject
 import java.math.BigDecimal
 import java.math.RoundingMode
-
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
+
 class FirstFragment : Fragment() {
-    fun createUrl(a: String,b: String,c: String): String {
-        return "https://currencyconverter.p.rapidapi.com/?from_amount=$a&from=$b&to=$c"
+    fun createUrl(old_amount: String,old_currency: String,new_currency: String): String {
+        return "https://currency-converter-by-api-ninjas.p.rapidapi.com/v1/convertcurrency?have=$old_currency&want=$new_currency&amount=$old_amount"
     }
 
-    fun createMsg(a: String,b: String): String {
-        return "$a EUR to $b USD."
+    fun createMsg(old_amount: String,new_amount: String, old_currency: String, new_currency: String ): String {
+        return "$old_amount $old_currency to $new_amount $new_currency."
     }
 
-    private fun apiCurrency(view: View) {
-        val kwota = view.findViewById<TextView>(R.id.podajKwote)
-        val urlString = createUrl(kwota.text.toString(),"EUR","USD")
-
+    fun connectApi(old_amount: String, old_currency: String, new_currency: String): JSONObject {
         val client = OkHttpClient()
-
         val request = Request.Builder()
-            .url(urlString.toString())
+            .url(createUrl(old_amount,old_currency,new_currency))
             .get()
-            .addHeader("x-rapidapi-host", "currencyconverter.p.rapidapi.com")
-            .addHeader("x-rapidapi-key", "NOT-AUTHORIZED")
+            .addHeader("x-rapidapi-host", "currency-converter-by-api-ninjas.p.rapidapi.com")
+            .addHeader("x-rapidapi-key", "9e7ed72733mshab0cd3c7461f069p17ef3bjsn9c88d40083eb")
             .build()
 
-        val response = client.newCall(request).execute()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {println("chujnia")}
+            override fun onResponse(call: Call, response: Response ) {
+                val answer = response.body?.string()
+                println(answer)
+                val jsonObj = JSONObject(answer?.substring(answer.indexOf("{"), answer.lastIndexOf("}") + 1))
+                return jsonObj
+            }
+        })
+
     }
 
-    private fun countMe(view: View) {
+    fun recalculate(view: View) {
 
-        val podanaKwota = view.findViewById<TextView>(R.id.podajKwote).text.toString()
-        var nowaKwota =BigDecimal(podanaKwota.toDouble() * 1.13).setScale(2, RoundingMode.HALF_EVEN)
-        view.findViewById<TextView>(R.id.kwotaPrzeliczona).text = createMsg(podanaKwota, nowaKwota.toString())
+        val old_amount = view.findViewById<TextView>(R.id.podajKwote).text.toString()
+        val old_currency = "EUR"
+        val new_currency = "USD"
+
+        val jsonObj = connectApi(old_amount, old_currency, new_currency )
+        val new_amount = jsonObj.getDouble("new_amount").toString()
+        val new_currency= jsonObj.getString("new_currency")
+        val old_currency= jsonObj.getString("old_currency")
+        val old_amount= jsonObj.getDouble("old_amount").toString()
+
+        view.findViewById<TextView>(R.id.kwotaPrzeliczona).text = createMsg(old_amount, new_amount, old_currency, new_currency)
 
         // 30 sekund to trochę długi czas oczekiwania, ale gdyby miało być 30 to tutaj dalibyśmy wartość 30000 zamiast 5000
         Thread.sleep(5000);
         view.findViewById<TextView>(R.id.kwotaPrzeliczona).isVisible = true
-
     }
 
     private var _binding: FragmentFirstBinding? = null
@@ -79,8 +85,7 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.przeliczButton).setOnClickListener {
-            countMe(view)
-
+            recalculate(view)
         }
     }
 
@@ -89,4 +94,5 @@ override fun onDestroyView() {
         _binding = null
     }
 }
+
 
